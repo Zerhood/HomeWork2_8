2a7f62ed380a1f535840b1ef16f8369d340adac5
Zerhood <Zerhood111@ya.ru>
2024-03-20 10:19:18 +0700

    rework Basket class

--- src/main/java/skypro/repository/Basket.java
+++ src/main/java/skypro/repository/Basket.java
@@ -1,18 +1,17 @@
 package skypro.repository;
 
 import lombok.Data;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Component
-@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@SessionScope
 public class Basket {
-    List<Integer> list;
+    private List<Integer> list;
 
     public Basket() {
         list = new ArrayList<>();
